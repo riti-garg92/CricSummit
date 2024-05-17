36cfc708002a54ec44b7ca8a071c2a7cf08090ed
riti-garg92 <167803970+riti-garg92@users.noreply.github.com>
2024-05-17 23:34:04 +0530

    2nd

--- app/src/main/java/p/ritika/cricsummit/MainActivity.kt
+++ app/src/main/java/p/ritika/cricsummit/MainActivity.kt
@@ -3,17 +3,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import p.ritika.cricsummit.Challenge1Activity
-import p.ritika.cricsummit.Challenge3Activity
 import p.ritika.cricsummit.ui.theme.CricSummitTheme
 
 class MainActivity : ComponentActivity() {
